ade275f9b667293e60a3091c8178bc2c901eabcc
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-13 10:29:29 -0400

    Admin Add Product to Database

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,12 +6,13 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
+import { addProduct } from "../../_actions/products";
 
 export default function ProductForm() {
   const [priceInCents, setPriceInCents] = useState<number>();
 
   return (
-    <form className="space-y-">
+    <form action={addProduct} className="space-y-">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
         <Input type="text" id="name" name="name" required />
