55ae007b8f527ebf402b8220550a2a20092abab4
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-13 09:29:13 -0400

    Admin Products Table

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,11 @@
 import { Nav, NavLink } from "@/components/Nav";
 import React from "react";
 
+// force next.js to not cache admin pages
+// admins will usually have fast internet, admins want most updated data
+// always dynamiccally generated -> BAD FOR USERS
+export const dynamic = "force-dynamic";
+
 export default function AdminLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
