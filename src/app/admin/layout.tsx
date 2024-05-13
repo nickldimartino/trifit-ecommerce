ddd59d03652ed393fae450709d97d4d4de760c25
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-13 08:34:35 -0400

    Navbar

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import { Nav, NavLink } from "@/components/Nav";
+import React from "react";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/admin">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/admin/users">Customers</NavLink>
+        <NavLink href="/admin/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6">{children}</div>
+    </>
+  );
+}
