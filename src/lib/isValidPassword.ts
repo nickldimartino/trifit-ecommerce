a0babf071bdbb3d590a6617a1e13a3d4cdb24efe
Nick DiMartino <nick.l.dimartino@gmail.com>
2024-05-14 08:45:51 -0400

    Home Page

--- src/lib/isValidPassword.ts
+++ src/lib/isValidPassword.ts
@@ -2,7 +2,6 @@ export async function isValidPassword(
   password: string,
   hashedPassword: string
 ) {
-  console.log(await hashPassword(password))
   return (await hashPassword(password)) === hashedPassword;
 }
 
